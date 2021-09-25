feb890903e35e0975f7f3d78bf963347eb365a2b
chenhonzhou <chenhonzhou@gmail.com>
2021-09-25 18:12:03 +0800

    fix: app can install function
    if app not install, `stderr` result not => ''

--- lib/utils/dpkg.dart
+++ lib/utils/dpkg.dart
@@ -1,21 +1,36 @@
 import 'dart:io';
 
 class DpkgUtil {
-
   /// check app can install [pkgName]
-  /// 
+  ///
   /// if not install.
   /// `stdout` just like
-  /// 
+  ///
   /// ```shell
   /// $ dpkg -l macosbigsur-cursorsd
   /// dpkg-query: no packages found matching macosbigsur-cursorsd
   /// ```
-  static bool appCanInstalled(String pkgName) {
-    var _ = Process.runSync("dpkg", [
-      "-l",
-      pkgName,
-    ],).stdout.toString();
-    return !_.contains("no packages found matching");
+  static bool appCanInstalled(String? pkgName) {
+    if (pkgName == null) return false;
+    var _ = Process.runSync(
+      "dpkg",
+      [
+        "-s",
+        pkgName,
+      ],
+    );
+    //.stdout.toString();
+    return (_.stderr.toString() == "");
+    // return !_.contains("no packages found matching");
+  }
+
+  static bool uninstall(String? pkgname) {
+    // TODO
+    return false;
+  }
+
+  static bool install(String? pkgname) {
+    // TODO
+    return false;
   }
 }
